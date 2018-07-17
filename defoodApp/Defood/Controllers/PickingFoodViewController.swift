6aa7d917cbb07b341250f706d77420f1d4205405
Alizandro Lopez <alizanpez@gmail.com>
2018-07-17 15:19:08 -0700

    Added text field for type of food and created the outlets

---
+++ defoodApp/Defood/Controllers/PickingFoodViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PickingFoodViewController.swift
+//  Defood
+//
+//  Created by Alizandro Lopez on 7/17/18.
+//  Copyright © 2018 GLWR. All rights reserved.
+//
+
+import UIKit
+
+class PickingFoodViewController: UIViewController {
+
+    @IBOutlet weak var pickingFoodTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let restaurantType = pickingFoodTextField.text
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
